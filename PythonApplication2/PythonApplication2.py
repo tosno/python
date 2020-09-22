f0eb97902497ce6b3e881b6c68655acd40ad27dc
Denis Nelen <tosno.nel@outlook.com>
2020-09-22 13:41:51 +0300

    Структуры данных

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,1 +1,11 @@
-
+import sys
+inteface = input('Интерфейс: ')
+vlan = input('Вилан: ')
+access_template = ['switch mode access',
+                   'switchport access vlan {}',
+                   'switchport nonegotiate',
+                   'spanning-tree portfast',
+                   'spanning-tree bpduguard enable']
+print('\n' + '-' * 30)
+print('interface {}'.format(inteface))
+print('\n'.join(access_template).format(vlan))
