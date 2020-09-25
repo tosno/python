7785215ec46eed4cd4ffc7c04642823c8b91deaf
Denis Nelen <tosno.nel@outlook.com>
2020-09-25 16:20:19 +0300

    Форматирование строк

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,11 +1,15 @@
-import sys
-inteface = input('Интерфейс: ')
-vlan = input('Вилан: ')
-access_template = ['switch mode access',
-                   'switchport access vlan {}',
-                   'switchport nonegotiate',
-                   'spanning-tree portfast',
-                   'spanning-tree bpduguard enable']
-print('\n' + '-' * 30)
-print('interface {}'.format(inteface))
-print('\n'.join(access_template).format(vlan))
+ospf_route = "       10.0.24.0/24 [110/41] via 10.0.13.3, 3d18h, FastEthernet0/0"
+ospf_value = ospf_route.strip().split()
+ospf_value.remove('via')
+template = """
+Prefix                {}
+AD/Metric             {}
+Next-Hop              {}
+Last update           {}
+Outbound Interface    {}
+"""
+print(template.format(ospf_value[0], ospf_value[1], ospf_value[2], ospf_value[3], ospf_value[4]))
+
+
+
+
