806253603cefb6274ce0ae649847cf64ea04f086
Denis Nelen <tosno.nel@outlook.com>
2020-10-06 11:39:02 +0300

    цикл for

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,22 +1,5 @@
-access_template = [
-    "Введите номер VLAN",
-    "switchport mode access", "switchport access vlan {}",
-    "switchport nonegotiate", "spanning-tree portfast",
-    "spanning-tree bpduguard enable"
-]
-trunk_template = [
-    "Введите разрешенные VLANы",
-    "switchport trunk encapsulation dot1q", "switchport mode trunk",
-    "switchport trunk allowed vlan {}"
-]
-config = {
-    'access': access_template,
-    'trunk': trunk_template
-    }
-mode = input('Введите режим работы интерфейса ({}): '.format('/'.join(list(config.keys()))))
-int = input('Введите тип и номер интерфейса: ')
-vlan = input('{}: '.format(config[mode][0]))
-config_str = '\n'.join(config[mode][1:])
-print('interface {}'.format(int))
-print(config_str.format(vlan))
-
+macs = ["aabb:cc80:7000", "aabb:dd80:7340", "aabb:ee80:7000", "aabb:ff80:7000"]
+mac_cisco = []
+for mac in macs:
+    mac_cisco.append(mac.replace(':', '.'))
+print(mac_cisco)
