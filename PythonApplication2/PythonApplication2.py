6c97683c0daa0e08a77895efe8999b7e72665607
Denis Nelen <tosno.nel@outlook.com>
2020-10-08 16:53:18 +0300

    config_trunk

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,5 +1,27 @@
-macs = ["aabb:cc80:7000", "aabb:dd80:7340", "aabb:ee80:7000", "aabb:ff80:7000"]
-mac_cisco = []
-for mac in macs:
-    mac_cisco.append(mac.replace(':', '.'))
-print(mac_cisco)
+access_template = [
+    "switchport mode access",
+    "switchport access vlan",
+    "spanning-tree portfast",
+    "spanning-tree bpduguard enable",
+]
+trunk_template = [
+    "switchport trunk encapsulation dot1q",
+    "switchport mode trunk",
+    "switchport trunk allowed vlan",
+]
+access = {"0/12": "10", "0/14": "11", "0/16": "17", "0/17": "150"}
+trunk = {"0/1": ["add", "10", "20"], "0/2": ["only", "11", "30"], "0/4": ["del", "17"]}
+for intf, vlan in trunk.items():
+    print("interface FastEthernet" + intf)
+    for command in trunk_template:
+        if command.endswith("allowed vlan"):
+            if vlan[0] == 'add':
+                print(f" {command} add {','.join(vlan[1:])}")
+            elif vlan[0] == 'only':
+                print(f" {command} {','.join(vlan[1:])}")
+            elif vlan[0] == 'del':
+                print(f" {command} remove {','.join(vlan[1:])}")
+            else:
+                pass
+        else:
+            print(f" {command}")
