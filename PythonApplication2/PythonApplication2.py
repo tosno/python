3dd42e17c369917c3b7ec728f0678dc7dae2a9f9
Denis Nelen <tosno.nel@outlook.com>
2020-10-12 16:25:58 +0300

    циклы, файлы

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,27 +1,9 @@
-access_template = [
-    "switchport mode access",
-    "switchport access vlan",
-    "spanning-tree portfast",
-    "spanning-tree bpduguard enable",
-]
-trunk_template = [
-    "switchport trunk encapsulation dot1q",
-    "switchport mode trunk",
-    "switchport trunk allowed vlan",
-]
-access = {"0/12": "10", "0/14": "11", "0/16": "17", "0/17": "150"}
-trunk = {"0/1": ["add", "10", "20"], "0/2": ["only", "11", "30"], "0/4": ["del", "17"]}
-for intf, vlan in trunk.items():
-    print("interface FastEthernet" + intf)
-    for command in trunk_template:
-        if command.endswith("allowed vlan"):
-            if vlan[0] == 'add':
-                print(f" {command} add {','.join(vlan[1:])}")
-            elif vlan[0] == 'only':
-                print(f" {command} {','.join(vlan[1:])}")
-            elif vlan[0] == 'del':
-                print(f" {command} remove {','.join(vlan[1:])}")
-            else:
-                pass
+with open('cisco_sw.txt') as src, open('result.txt', 'w') as dest:
+    for line in src:
+        if line.strip().endswith('portfast') or line.strip().endswith('trap'):
+            continue
         else:
-            print(f" {command}")
+            print(line.rstrip())
+            dest.write(line)
+
+
