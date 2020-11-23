b6db61a50b68ce5d3714fafb28fd844e3267297c
Denis Nelen <tosno.nel@outlook.com>
2020-11-23 14:09:42 +0300

    Парсер вланов

--- PythonApplication2/PythonApplication2.py
+++ PythonApplication2/PythonApplication2.py
@@ -1,23 +1,27 @@
-trunk_mode_template = [
-    "switchport mode trunk", "switchport trunk native vlan 999",
-    "switchport trunk allowed vlan"
-]
-trunk_config = {
-    "FastEthernet0/1": [10, 20, 30],
-    "FastEthernet0/2": [11, 30],
-    "FastEthernet0/4": [17]
-}
-def generate_access_config(intf_vlan_mapping, trunk_template):
-    config = dict.fromkeys(intf_vlan_mapping.keys())
-    for intf, vlans in intf_vlan_mapping.items():
-        value = []
-        for command in trunk_template:
-            if command.endswith("allowed vlan"):
-                value.append(command + ' ' + str(vlans).strip('[]'))
-            else:
-                value.append(command)
-        config[intf] = value
-    return config
-print(generate_access_config(trunk_config, trunk_mode_template))
-
+def get_int_vlan_map(config_filename):
+    mode_access = {}
+    mode_trunk = {}
+    with open(config_filename) as f:
+        for line in f:
+            if "Ethernet" in line:
+                if "vlans" in locals():
+                    if len(vlans) > 0:
+                        result = [vlan for vlan_list in vlans for vlan in vlan_list] #преобразование в список STR
+                        convert = []
+                        for vl in result:
+                            if "-" in vl:
+                                vl_range = [num for num in range(int(vl.split('-')[0]), int(vl.split('-')[1])+1)]
+                                for i in vl_range:
+                                    convert.append(int(i))
+                            else:
+                                convert.append(int(vl))
+                        mode_trunk[interface] = convert
+                interface = line.split()[1]
+                vlans = [] #список создается при совпадении "Ethernet"
+            if "access vlan" in line:
+                mode_access[interface] = int(line.split()[-1])
+            if "allowed vlan" in line:
+                vlans.append(line.split()[-1].split(',')) #формируется список списков для соответствующего INTERFACE
+    return mode_trunk
+print(get_int_vlan_map('cisco_config.txt'))
 
